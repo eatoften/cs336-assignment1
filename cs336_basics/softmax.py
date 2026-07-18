5224503636742939829c8dc7fee59b839451c27c
Yijia Lai <1224537317@qq.com>
2026-07-18 15:32:18 +0800

    Implemented softmax

--- cs336_basics/softmax.py
+++ cs336_basics/softmax.py
@@ -1,4 +1,10 @@
 import torch
 from torch import nn
 
-class
+def softmax(x:torch.Tensor,dim):
+    m = torch.amax(x,dim,keepdim = True)
+    shifted = x - m
+    numerator = torch.exp(shifted)
+    denominator = torch.sum(numerator,dim=dim,keepdim=True)
+    output = numerator/denominator
+    return output
