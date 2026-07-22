903a2f839a1e74ec3363b355a23ae6252bf3e894
Yijia Lai <1224537317@qq.com>
2026-07-22 22:46:25 +0800

    Added KVcache to multihead_attention

--- cs336_basics/multihead_attention.py
+++ cs336_basics/multihead_attention.py
@@ -24,7 +24,7 @@ class CausalMultiHeadSelfAttention(nn.Module):
         self.v_proj = Linear(d_model,d_model,device=device,dtype=dtype)
         self.output_proj = Linear(d_model,d_model,device=device,dtype=dtype)
 
-    def forward(self, x:torch.Tensor, token_positions=None):
+    def forward(self, x:torch.Tensor, token_positions=None,past_key_value=None,use_cache=False):
         #x,Q,K,V: (...,sequence,d_model)
         Q = self.q_proj(x)
         K = self.k_proj(x)
@@ -34,20 +34,46 @@ class CausalMultiHeadSelfAttention(nn.Module):
         V = rearrange(V,"... seq (head d_head) -> ... head seq d_head",head = self.num_heads,d_head = self.d_head)
         seq_len = x.shape[-2]
 
+        if past_key_value is None:
+            past_len = 0
+        else:
+            past_K, past_V = past_key_value
+            past_len = past_K.shape[-2]
+
         if self.rope is not None:
             if token_positions is None:
-                token_positions = torch.arange(0, seq_len,device=x.device)
+                token_positions = torch.arange(past_len, past_len+seq_len,device=x.device)
             token_positions = rearrange(token_positions,"... seq -> ... 1 seq")
             Q = self.rope(Q,token_positions)
             K = self.rope(K,token_positions)
 
-        mask = torch.ones(seq_len,seq_len,dtype=torch.bool,device=x.device)
-        mask = torch.tril(mask,diagonal=0)
-        result = scaled_dot_product_attention(Q,K,V,mask)
+        if past_key_value is None:
+            K_all = K
+            V_all = V
+        else:
+            K_all = torch.cat((past_K, K), dim=-2)
+            V_all = torch.cat((past_V, V), dim=-2)
+
+        present_key_value = (K_all, V_all)
+
+
+        if past_key_value is None:
+            mask = torch.ones(seq_len,seq_len,dtype=torch.bool,device=x.device)
+            mask = torch.tril(mask,diagonal=0)
+        else:
+            if seq_len != 1:
+                raise ValueError("Invalid seq_len")
+            else:
+                mask = None
+        
+        result = scaled_dot_product_attention(Q,K_all,V_all,mask)
         #result: (..., head, sequence, d_head)
         result = rearrange(result,"... head seq d -> ... seq (head d)")
         result = self.output_proj(result)
-        return result
+        if use_cache == False:
+            return result
+        else:
+            return result, present_key_value
 
 
 
